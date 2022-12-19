baae69bf0cd7dd5026ff69ea036b7784a46592b7
Hiren <rebeldevmurari@gmail.com>
2022-12-19 22:36:50 +0000

    Initial commit

---
+++ src/main/java/com/rebel/DataPersistenceDemo/DataPersistenceDemoApplication.java
@@ -0,0 +1,13 @@
+package com.rebel.DataPersistenceDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataPersistenceDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataPersistenceDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rebel/DataPersistenceDemo/DataPersistenceDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rebel.DataPersistenceDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataPersistenceDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
