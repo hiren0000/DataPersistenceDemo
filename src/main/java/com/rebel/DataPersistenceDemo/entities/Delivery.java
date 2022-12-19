96ff142138365b7014dc66cf1005dd0883a6a21f
Hiren <rebeldevmurari@gmail.com>
2022-12-19 23:30:26 +0000

    Created two entities using JPA

---
+++ src/main/java/com/rebel/DataPersistenceDemo/entities/Delivery.java
@@ -0,0 +1,73 @@
+package com.rebel.DataPersistenceDemo.entities;
+
+import org.hibernate.annotations.Nationalized;
+import org.hibernate.annotations.Type;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
+@Entity
+public class Delivery
+{
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Nationalized
+    private String name;
+
+    @Column(name = "address_full", length = 500)
+    private String address;
+
+    private LocalDateTime deliveryTime;
+    @Type(type = "yes_no")
+    private Boolean completed;
+
+    public Delivery() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public LocalDateTime getDeliveryTime() {
+        return deliveryTime;
+    }
+
+    public void setDeliveryTime(LocalDateTime deliveryTime) {
+        this.deliveryTime = deliveryTime;
+    }
+
+    public Boolean getCompleted() {
+        return completed;
+    }
+
+    public void setCompleted(Boolean completed) {
+        this.completed = completed;
+    }
+}
